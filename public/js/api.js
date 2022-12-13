4b2f7bf6d7ca9db7fd38a45bdc786dcc87ba2937
Jonatha Fernandes <101746582+jonathafernandes@users.noreply.github.com>
2022-12-13 01:03:32 -0300

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+let api_key = "01d5e03a2d3d32373601cc4f56e805a0"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
